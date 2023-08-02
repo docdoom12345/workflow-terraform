5679983983bf792e40c3337ae7c66b8ee6ca11e3
docdoom12345 <103731163+docdoom12345@users.noreply.github.com>
2023-08-02 15:43:34 +0530

    Update main_test.go

--- test/main_test.go
+++ test/main_test.go
@@ -2,12 +2,8 @@ package test
 
 import (
 	"testing"
-	"os/exec"
-	"bufio"
-	"os"
-	"strings"
 	"path/filepath"
-	//"io/ioutil"
+	"io/ioutil"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
@@ -19,44 +15,11 @@ func TestTerraformPlanToFile(t *testing.T) {
 	terraformOptions := &terraform.Options{
 		// Set the path to your Terraform code that will be tested.
 		TerraformDir: "../",
-		PlanFilePath: "terraform.tfplan",
 	}
 
-	// Run `terraform init` and `terraform plan` to generate the plan.
-	terraform.Init(t, terraformOptions)
-        currentDir, err := os.Getwd()
-	PlanFilePath := filepath.Dir(currentDir)
-	PlanFilePath += "/terraform.tfplan"
-	// Get the plan using `terraform plan -out` command.
-	cmd := exec.Command("terraform", "plan", "-out", PlanFilePath)
-	cmd.Dir = terraformOptions.TerraformDir
-	filePath := cmd.Run()
-	if filePath != nil {
-		t.Fatalf("Failed to run terraform plan command: %v", filePath)
-	}
-	planFile, err := os.Open(terraformOptions.PlanFilePath)
-	if err != nil {
-		t.Fatalf("Failed to open plan file: %v", err)
-	}
-	defer planFile.Close()
-	vmName := ""
-	scanner := bufio.NewScanner(planFile)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.Contains(line, "azurerm_windows_virtual_machine.example") {
-			// Assuming "azurerm_virtual_machine.example_vm" is the resource name in your Terraform configuration.
-			// Adjust it accordingly based on your actual resource name.
-			// The VM name will be in the line following the resource name.
-			scanner.Scan()
-			vmName = scanner.Text()
-			break
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		t.Fatalf("Error while scanning plan file: %v", err)
-	}
-
-	// At this point, the VM name should be stored in the "vmName" variable.
-	t.Logf("VM Name: %s", vmName)
-}
+	// Run `terraform init` and `terraform plan` to generate the plan
+	parentDir := ".."
+	filePath := filepath.Join(parentDir,"terraform.plan")
+	plan, err := terraform.InitandPlan(t, terraformOptions)
+	
+        }
