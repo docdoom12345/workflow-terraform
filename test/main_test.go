26c6b85be27aab4e37a9df33d79b8add2c3e0122
docdoom12345 <103731163+docdoom12345@users.noreply.github.com>
2023-08-02 16:33:08 +0530

    Update main_test.go

--- test/main_test.go
+++ test/main_test.go
@@ -2,7 +2,7 @@ package test
 
 import (
 	"testing"
-	//"os/exec"
+	"os/exec"
 	//"bufio"
 	"os"
 	"fmt"
@@ -28,10 +28,25 @@ func TestTerraformPlanToFile(t *testing.T) {
 	// Get the plan using `terraform plan -out` command.
 	PlanFileName := "../terraform.tfplan"
 	terraform.RunTerraformCommand(t, terraformOptions, "plan" ,"-out="+PlanFileName)
-	content, err :=ioutil.ReadFile(PlanFileName)
-	if err != nil {
-            fmt.Println("Unable to read file")
-	    os.Exit(1)
-        }
-	fmt.Println(string(content))
+	currentDir, err := os.Getwd()
+	    if err != nil {
+		fmt.Println("Error getting the current directory:", err)
+		os.Exit(1)
+	    }
+	
+	    // Navigate to the parent directory
+	    parentDir := filepath.Join(currentDir, "..")
+	
+	    // Define the file path to read
+	    filePath := filepath.Join(parentDir, "terraform.tfplan")
+	
+	    // Read the entire content of the file
+	    content, err := ioutil.ReadFile(filePath)
+	    if err != nil {
+		fmt.Println("Error reading the file:", err)
+		os.Exit(1)
+	    }
+	
+	    // Convert the content to a string and print it
+	    fmt.Println(string(content))
 }
